d0c59c79d7ea17a6bffbe33c788ab507716e75e0
spiknumb <spiknumb@netscape.net>
2021-12-18 14:08:48 -0500

    adding necessary divs with variable height and width

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 let howMany = 16;
 
-let grid = [];
-
 const outerBox = document.querySelector(".box");
+const totalWidth = outerBox.scrollHeight;
 
-for (let i = 1; i <= howMany; i++) {
+// for (let i = 1; i <= howMany; i++) {
 
-    let oneRow = document.createElement('DIV');
-    oneRow.classList = ('row'); 
-    outerBox.appendChild(oneRow);
+//     let oneRow = document.createElement('DIV');
+//     oneRow.classList = (`row${i} row`); 
+//     outerBox.appendChild(oneRow);
 
-}
+//     addInnerBoxes(i);
 
-let boxRows = document.querySelectorAll(".row")
+// }
 
-for (item in boxRows) {
+for (let i = 1; i <= howMany * howMany; i++) {
 
     let innerBox = document.createElement("DIV");
-    innerBox.classList.add("innerBox");
-    item.appendChild(innerBox);
+    innerBox.classList = (`innerBox${i} innerbox`);
+    innerBox.style.width=`${totalWidth/howMany}px`;
+    innerBox.style.height=`${totalWidth/howMany}px`;
+    outerBox.appendChild(innerBox);
 
 }
 
-//let i = boxRows[0]; i <= boxRows.length; i++
+// function addInnerBoxes(i) {
+
+//     for (let a = 1; a <= howMany; a++) {
+
+//         let oneRow = document.querySelector(`.row${i}`)
+//         let innerBox = document.createElement("DIV");
+//         innerBox.classList = (`innerBox${a} innerbox`);
+//         oneRow.clientHeight = totalWidth / howMany;
+//         oneRow.clientWidth = totalWidth / howMany;
+//         oneRow.appendChild(innerBox);
+
+//     }
+
+// }
