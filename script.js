4e6ae091bb624269c460e25834ceb787582c3435
spiknumb <spiknumb@netscape.net>
2021-12-19 11:43:16 -0500

    added table reset functionality

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 let howMany = 16;
-
 const outerBox = document.querySelector(".box");
 const totalWidth = outerBox.scrollHeight;
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
-for (let i = 1; i <= howMany * howMany; i++) {
+setTable(howMany);
 
-    let innerBox = document.createElement("DIV");
-    innerBox.classList = (`innerBox${i} innerBox`);
-    innerBox.style.width=`${totalWidth/howMany}px`;
-    innerBox.style.height=`${totalWidth/howMany}px`;
-    outerBox.appendChild(innerBox);
+button.addEventListener('click', function () { resetTable(input.value) } );
+
+function resetTable(value) {
+
+    if (+value) {
+
+        outerBox.innerHTML = ''
+        setTable(+value);
+
+    } else {
+
+        alert("Please enter a real number");
+
+    }
+}
+
+function setTable(value) {
+    
+    for (let i = 1; i <= value * value; i++) {
+
+        let innerBox = document.createElement("DIV");
+        innerBox.classList = (`innerBox${i} innerBox`);
+        innerBox.style.width=`${totalWidth/value}px`;
+        innerBox.style.height=`${totalWidth/value}px`;
+        outerBox.appendChild(innerBox);
+
+    }
 
 }
+
+
+
 
 
 
