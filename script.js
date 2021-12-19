71f1e135bad533b08e66f4e05fcdc6a6caffee0f
spiknumb <spiknumb@netscape.net>
2021-12-19 08:15:48 -0500

    added text input field and button

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const totalWidth = outerBox.scrollHeight;
 for (let i = 1; i <= howMany * howMany; i++) {
 
     let innerBox = document.createElement("DIV");
-    innerBox.classList = (`innerBox${i} innerbox`);
+    innerBox.classList = (`innerBox${i} innerBox`);
     innerBox.style.width=`${totalWidth/howMany}px`;
     innerBox.style.height=`${totalWidth/howMany}px`;
     outerBox.appendChild(innerBox);
 
 }
+
+
 
